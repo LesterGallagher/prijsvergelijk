2fcce41981e8ace000d408635416c593cde7b552
LesterGallagher <LesterGallagher@gmail.com>
2018-09-27 17:21:24 +0200

    initial

---
+++ about.md
@@ -0,0 +1,10 @@
+---
+title: About
+permalink: "/about/"
+layout: page
+greytitle: us
+description: Echo Sierra Studio's about page. Learn more about us.
+---
+
+My main focus when I'm writing a web-app, is to make the web-app responsive, learn about new tools and trying to use new techniques. SPA's, Build tools and other front-end tools are things i would like to learn more about. I mostly end up using some sort of combination of Node js tools to build the front-end of a web-app.
+
---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+$(function () {
+    $('.daisyconfeed').each(function () {
+        daisyconFeed(this.getAttribute('data-params'), this);
+    })
+});
+
+function daisyconFeed(params, container) {
+    var url = 'https://ess-server.herokuapp.com/daisycon-proxy/' + params;
+    $.ajax({
+        url: url,
+        data: {},
+        success: processFeed,
+        error: error,
+        type: "GET",
+        dataType: 'json',
+    });
+
+    function error(err) {
+        console.error(err);
+    }
+
+    function processFeed(data, status, xhr) {
+        var products = data.datafeed.programs[0].products;
+        for(var i = 0; i < products.length; i++) {
+            (function(product) {
+                $(container).append($(
+'<a class="col col-xxs-12 col-xs-6 col-sm-4 col-lg-3" target="_blank" href="' + product.product_info.link + '"><article class="panel panel-primary">\
+<div class="panel-heading">' + product.product_info.title + '</div>\
+<img alt="' + product.product_info.images[0].type + '" \
+src="' + product.product_info.images[0].location + '" \
+style="width: 100%" class="img-responsive">\
+<div class="panel-body">' + product.product_info.description + '</div>\
+</article></a>'
+                ));
+            })(products[i]);
+        }
+    }
+}
