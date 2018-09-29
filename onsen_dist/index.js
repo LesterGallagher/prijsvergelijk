de04cba15547c02e703f0346af2347c269ff4d5a
LesterGallagher <LesterGallagher@gmail.com>
2018-09-29 21:48:48 +0200

    iframes

--- onsen_dist/index.js
+++ onsen_dist/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 fs.readdirSync('../vergelijken').forEach(file => {
     fs.writeFileSync('iframe-' + file, `---
+layout: vergelijker-iframe
 ---
 {% assign page = site.pages | where: "name", "${file}" | first %}
 {{ page.content }}`);
