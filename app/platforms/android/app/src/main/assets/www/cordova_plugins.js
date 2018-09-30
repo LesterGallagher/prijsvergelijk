55aa341d9f684a4605d1f4228f966c109f3aa9a3
LesterGallagher <LesterGallagher@gmail.com>
2018-09-30 14:05:54 +0200

    fixes

--- app/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ app/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-plugin-admobpro.AdMob",
+    "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+    "pluginId": "cordova-plugin-admobpro",
+    "clobbers": [
+      "window.AdMob"
+    ]
+  },
+  {
     "id": "cordova-plugin-device.device",
     "file": "plugins/cordova-plugin-device/www/device.js",
     "pluginId": "cordova-plugin-device",
@@ -25,11 +33,11 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-admobpro.AdMob",
-    "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
-    "pluginId": "cordova-plugin-admobpro",
+    "id": "cordova-plugin-headercolor.HeaderColor",
+    "file": "plugins/cordova-plugin-headercolor/www/HeaderColor.js",
+    "pluginId": "cordova-plugin-headercolor",
     "clobbers": [
-      "window.AdMob"
+      "cordova.plugins.headerColor"
     ]
   },
   {
@@ -39,27 +47,19 @@ module.exports = [
     "clobbers": [
       "window.StatusBar"
     ]
-  },
-  {
-    "id": "cordova-plugin-headercolor.HeaderColor",
-    "file": "plugins/cordova-plugin-headercolor/www/HeaderColor.js",
-    "pluginId": "cordova-plugin-headercolor",
-    "clobbers": [
-      "cordova.plugins.headerColor"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-device": "2.0.2",
-  "cordova-plugin-dialogs": "2.0.1",
   "cordova-plugin-extension": "1.5.4",
   "cordova-plugin-admobpro": "2.31.6",
   "cordova-plugin-browsersync": "0.1.7",
+  "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-dialogs": "2.0.1",
+  "cordova-plugin-headercolor": "1.0",
   "cordova-plugin-statusbar": "2.4.2",
-  "cordova-plugin-headercolor": "1.0"
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
